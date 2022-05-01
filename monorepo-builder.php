d989a9199627c8e05173f8a867b3d628756abb39
Andriy <andriy.savchenko@gmail.com>
2022-05-01 15:01:20 -0400

    Workflow testing

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -8,6 +8,13 @@ use Symplify\MonorepoBuilder\ValueObject\Option;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
+
+    // where are the packages located?
+    $parameters->set(Option::PACKAGE_DIRECTORIES, [
+        // default value
+        __DIR__ . '/packages',
+    ]);
+
     // for "merge" command
     $parameters->set(Option::DATA_TO_APPEND, [
         ComposerJsonSection::REQUIRE_DEV => [
